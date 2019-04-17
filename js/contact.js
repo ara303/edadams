c0472d73220e45e446028e51473856ec8191826c
E. Adams <edadams@users.noreply.github.com>
2019-04-18 00:51:49 +0100

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,17 @@
-//...
+let form = document.querySelector('.form-contact');
+let XHR = new XMLHttpRequest();
+let doForm = function() {
+  let FD = new FormData(form);
+  XHR.addEventListener('load', function(e) {
+    document.querySelector('.form-success').classList.add('is-visible');
+  });
+  XHR.addEventListener('error', function() {
+    document.querySelector('.form-failure').classList.add('is-visible');
+  });
+  XHR.open('POST', form.getAttribute('action'));
+  XHR.send(FD);
+}
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  doForm();
+});
